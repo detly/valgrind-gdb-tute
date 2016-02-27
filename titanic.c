b9ec55b4cca6e7080849e291c6ba77ddbaca2740
Jason Heeris <jason.heeris@gmail.com>
2016-02-28 09:41:30 +1100

    Introduced typo, typo correction and buffer reuse.

--- titanic.c
+++ titanic.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static const char *LOG_TEXT = "miles, still unsinkable!\n";
+static const char *LOG_TEXT = "miles, stilluunsinkable!\n";
 
 unsigned count_digits(unsigned number)
 {
@@ -20,7 +20,14 @@ unsigned log_buffer_length(unsigned distance)
 
 full_steam_ahead(unsigned distance, char * captains_log)
 {
-    sprintf(captains_log, "%u %s", distance, LOG_TEXT);
+    if (distance > 1000)
+    {
+        sprintf(captains_log, ">1k miles, iffy!\n");
+    }
+    else
+    {
+        sprintf(captains_log, "%u %s", distance, LOG_TEXT);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -28,6 +35,8 @@ int main(int argc, char *argv[])
     /* ints only please, let's avoid float errors */
     int to_the_atlantic;
     int nautical_miles;
+    unsigned last_length = 0;
+    char *captains_log = NULL;
 
     if (argc < 2)
     {
@@ -45,14 +54,24 @@ int main(int argc, char *argv[])
     {
         const unsigned current_length = log_buffer_length(nautical_miles);
 
-        char *captains_log = malloc(current_length);
+        if (current_length > last_length)
+        {
+            free(captains_log);
+            captains_log = malloc(current_length);
+        }
 
         full_steam_ahead(nautical_miles, captains_log);
+        /* The exclamation mark makes us seem surprised. */
+        captains_log[current_length - 3] = '.';
+        /* Fix spelling error. */
+        captains_log[current_length - 14] = ' ';
 
         printf("%s", captains_log);
 
-        free(captains_log);
+        last_length = current_length;
     }
+
+    free(captains_log);
 
     return 0;
 }
